afe2b20027a9e7f9d8e3bfc45ecd0def4a5e05d8
yhh <1903432957@qq.com>
2022-10-30 13:28:44 +0800

    左侧分类结构渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="container">homePage</div>
+  <div class="container">
+    <!-- 首页入口(分类+广告) -->
+    <div class="home-entry">
+      <!-- 左侧分类 -->
+      <home-category></home-category>
+      <!-- 轮播图 -->
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeCategory from './components/home-category.vue'
 export default {
+  components: {
+    HomeCategory
+  },
   name: 'HomePage'
 }
 </script>
